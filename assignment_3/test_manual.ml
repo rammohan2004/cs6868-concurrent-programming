00abe39d8bed0dfd32625bfb1580c5ee7e5a1235
rammohan2004 <ramamohanraobotla12@gmail.com>
2026-04-03 15:08:47 +0530

    Implemented test sequential basic

--- assignment_3/test_manual.ml
+++ assignment_3/test_manual.ml
@@ -10,8 +10,58 @@ let assert_array_eq a b msg =
     exit 1
   end
 
-(** Test create, enq, deq, size, capacity in a single thread. *)
-let test_sequential_basic () = failwith "TODO: implement"
+let assert_int_eq a b msg =
+  if a <> b then begin
+    printf "FAIL: %s\n  expected: %d\n  got: %d\n" msg a b;
+    exit 1
+  end
+
+let assert_bool_eq expected actual msg =
+  if expected <> actual then begin
+    printf "FAIL: %s\n  expected: %b\n  got: %b\n" msg expected actual;
+    exit 1
+  end
+(* Test create, enq, deq, size, capacity in a single thread. *)
+let test_sequential_basic () = 
+  printf "Running test_sequential_basic...\n";
+  
+  let q = BatchQueue.create 5 in
+  
+  assert_int_eq 5 (BatchQueue.capacity q) "initial capacity";
+  assert_int_eq 0 (BatchQueue.size q) "initial size";
+  assert (BatchQueue.try_deq q 1 = None); 
+
+
+  assert_bool_eq true (BatchQueue.try_enq q [|10; 20; 30|]) "try_enq 3 items";
+  assert_int_eq 3 (BatchQueue.size q) "size after try_enq 3";
+
+  BatchQueue.enq q [|40; 50|];
+  assert_int_eq 5 (BatchQueue.size q) "size after enq 2 (queue exactly full)";
+
+  assert_bool_eq false (BatchQueue.try_enq q [|60|]) "try_enq 1 item when full";
+  assert_int_eq 5 (BatchQueue.size q) "size remains 5 after failed try_enq";
+
+  let res_full = BatchQueue.deq q 5 in
+  assert_array_eq [|10; 20; 30; 40; 50|] res_full "deq exactly 5 items";
+  assert_int_eq 0 (BatchQueue.size q) "size after emptying";
+
+  BatchQueue.enq q [|1; 2|];
+  assert_bool_eq true (BatchQueue.try_enq q [|3; 4; 5|]) "try_enq fill back to 5";
+  
+  let res_partial = BatchQueue.try_deq q 2 in
+  (match res_partial with
+   | Some arr -> assert_array_eq [|1; 2|] arr "try_deq 2 items"
+   | None -> failwith "FAIL: try_deq returned None unexpectedly");
+  assert_int_eq 3 (BatchQueue.size q) "size after try_deq 2";
+
+  assert (BatchQueue.try_deq q 4 = None);
+  assert_int_eq 3 (BatchQueue.size q) "size remains 3 after failed try_deq";
+
+  let res_rest = BatchQueue.deq q 3 in
+  assert_array_eq [|3; 4; 5|] res_rest "deq remaining 3";
+  assert_int_eq 0 (BatchQueue.size q) "final size is 0";
+
+  printf "PASSED\n"
 
 (** Test that invalid arguments raise [Invalid_argument]. *)
 let test_error_handling () = failwith "TODO: implement"
